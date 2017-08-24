a3f6f47f499413b2b6c141bde17202ce15ef57f0
Mayank <mayank.cse14@itbhu.ac.in>
2017-08-25 00:42:26 +0530

    Removed explicit C/C++ export lines

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm -rf build
-export CPLUS_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers
-export C_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers
+#If you get C/C++ headers missing errors, then uncomment the following 2 lines.
+#export CPLUS_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers
+#export C_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers
 swig -c++ -python example.i
 python setup.py install
